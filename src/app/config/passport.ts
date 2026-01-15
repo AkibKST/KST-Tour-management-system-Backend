ff66df5f2e0c21497845c585f7cb4c416d7dc315
AkibKST <akibkst22@gmail.com>
2026-01-15 15:54:55 +0600

    create email password login with passport

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import bcryptjs from "bcryptjs";
+import { Strategy as LocalStrategy } from "passport-local";
 import passport from "passport";
 import {
   Strategy as GoogleStrategy,
@@ -9,6 +11,59 @@ import { Role } from "../modules/user/user.interface";
 import { User } from "../modules/user/user.model";
 import { envVars } from "./env";
 
+//Local Strategy
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email: string, password: string, done) => {
+      try {
+        const isUserExist = await User.findOne({ email });
+
+        // if (!isUserExist) {
+        //     return done(null, false, { message: "User does not exist" })
+        // }
+
+        if (!isUserExist) {
+          return done("User does not exist");
+        }
+
+        const isGoogleAuthenticated = isUserExist.auths.some(
+          (providerObjects) => providerObjects.provider == "google"
+        );
+
+        if (isGoogleAuthenticated && !isUserExist.password) {
+          return done(null, false, {
+            message:
+              "You have authenticated through Google. So if you want to login with credentials, then at first login with google and set a password for your Gmail and then you can login with email and password.",
+          });
+        }
+
+        // if (isGoogleAuthenticated) {
+        //     return done("You have authenticated through Google. So if you want to login with credentials, then at first login with google and set a password for your Gmail and then you can login with email and password.")
+        // }
+
+        const isPasswordMatched = await bcryptjs.compare(
+          password as string,
+          isUserExist.password as string
+        );
+
+        if (!isPasswordMatched) {
+          return done(null, false, { message: "Password does not match" });
+        }
+
+        return done(null, isUserExist);
+      } catch (error) {
+        console.log(error);
+        done(error);
+      }
+    }
+  )
+);
+
+// Google OAuth Strategy
 passport.use(
   new GoogleStrategy(
     {
